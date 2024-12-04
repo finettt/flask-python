3ca7eb7539230829f10af901a11aff747a87990e
finettt <finetnew@gmail.com>
2024-12-04 08:37:33 +0600

    first commit

---
+++ app/main.py
@@ -0,0 +1,12 @@
+import flask
+from flask import request
+from flask import render_template
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8000)
