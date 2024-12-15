5d3206790e9a3cc19d97daa7ed2f69bbafbb2117
finettt <finetnew@gmail.com>
2024-12-15 13:18:17 +0600

    small fixes

--- app/main.py
+++ app/main.py
@@ -22,20 +22,29 @@ dictConfig({
         'format':'[%(asctime)s] [%(process)d] [%(levelname)s] %(message)s',
         'datefmt':'%Y-%m-%d %H:%M:%S %z',
     }},
-    'handlers': {'wsgi': {
+    'handlers': {
+        'wsgi': {
         'class': 'logging.StreamHandler',
         'stream': 'ext://flask.logging.wsgi_errors_stream',
         'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
+        },
+        'file': {
+        'class': 'logging.FileHandler',
+        'filename': 'app.log',  # The file where logs will be stored
+        'formatter': 'default',
+        },
+    },
+    'loggers':{
+        'root': {
+            'level': 'INFO',
+            'handlers': ['wsgi','file']
+        }
     }
 })
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.ejs',request=request.headers  )
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=8000)
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,11 +3,12 @@ import logging
 
 def pre_request(worker, req):
     logging.info("%s: %s" % (req.method, req.path))
+    for k, v in req.headers:
+        logging.info("%s: %s" % (k, v))
 
 bind = "127.0.0.1:8000"
 
 max_requests = 1000
 max_requests_jitter = 50
-
 log_file = "-"
-# workers =  multiprocessing.cpu_count() * 2 + 1
+#workers =  multiprocessing.cpu_count() * 2 + 1
